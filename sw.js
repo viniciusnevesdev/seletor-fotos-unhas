68e9487fd1b6eae1c2583184933235883cdf4d55
Vinicius <viniciusnevez123@gmail.com>
2026-09-08 19:23:47 -0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = "seletor-unhas-v01";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
